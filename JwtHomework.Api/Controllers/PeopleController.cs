e37f5b99538462b555ebd191d5a5eeb3516dc7e1
güven boydak <gvn.boydak@gmail.com>
2022-08-03 23:20:23 +0300

    gif

--- JwtHomework.Api/Controllers/PeopleController.cs
+++ JwtHomework.Api/Controllers/PeopleController.cs
@@ -10,7 +10,7 @@ namespace JwtHomework.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class PeopleController : CustomBaseController
     {
         private readonly IPersonService _personService;
